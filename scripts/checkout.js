d35dcd496392852e563eec094c636339ec3f4151
mominAman7860 <amanchandsomomin7860@gmail.com>
2024-12-31 21:25:51 +0530

    14-e

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart , removeFromCart} from '../data/cart.js';
+import {cart , removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -112,10 +112,7 @@ document.querySelectorAll('.js-delete-link').forEach ( (link) => {
 updateCartQuantity();
 
 function updateCartQuantity () {
-    let cartQuantity = 0;
-        cart.forEach((cartItem) => {
-            cartQuantity += cartItem.quantity;
-        });
+    let cartQuantity = calculateCartQuantity();
 
         document.querySelector('.js-checkout-header-middle-section').innerHTML = `${cartQuantity} items`;
 }
