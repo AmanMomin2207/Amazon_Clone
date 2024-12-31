b9ab60f0dbff0a267e32a24dc79221ffe784451c
mominAman7860 <amanchandsomomin7860@gmail.com>
2024-12-31 21:48:57 +0530

    14-h

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -115,7 +115,8 @@ document.querySelectorAll('.js-delete-link').forEach ( (link) => {
 document.querySelectorAll('.js-update-link').forEach ( (link) => {
     link.addEventListener('click' , () => {
         const productId = link.dataset.productId;
-        console.log(productId);
+        const container = document.querySelector('.js-cart-item-container');
+        container.classList.add('is-editing-quantity');
     });
 })
 updateCartQuantity();
