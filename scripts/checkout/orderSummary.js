c1db85bd60525cfcdf81ac8bfd6ce36665a9fe01
mominAman7860 <amanchandsomomin7860@gmail.com>
2025-01-01 15:06:10 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,13 +4,14 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions , getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
-hello();
-// console.log(dayjs());
-const today = dayjs();
-const deliveryDate = today.add(7 , 'days');
-console.log(deliveryDate.format('dddd , MMMM D'));
+// hello();
+// // console.log(dayjs());
+// const today = dayjs();
+// const deliveryDate = today.add(7 , 'days');
+// console.log(deliveryDate.format('dddd , MMMM D'));
 
 export function renderOrderSummary() {
 
@@ -127,6 +128,8 @@ export function renderOrderSummary() {
 
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderPaymentSummary();
             updateCartQuantity();
         });
     });
@@ -136,6 +139,7 @@ export function renderOrderSummary() {
             const productId = link.dataset.productId;
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.classList.add('is-editing-quantity');
+            renderPaymentSummary();
         });
     });
 
@@ -159,6 +163,8 @@ export function renderOrderSummary() {
             quantityLabel.innerHTML = ValueInput;
 
             updateCartQuantity();
+
+            renderPaymentSummary();
         });
     });
 
@@ -177,6 +183,7 @@ export function renderOrderSummary() {
                 const {productId , deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId , deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
     });
 
