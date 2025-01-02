0c911952809d5726e87a9c236b67ae4b018322a6
mominAman7860 <amanchandsomomin7860@gmail.com>
2025-01-02 22:21:57 +0530

    Load products beforeAll hook

--- tests-jasmine/checkout/orderSummayTest.js
+++ tests-jasmine/checkout/orderSummayTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import {loadFromStorage} from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary' , () => {
 
