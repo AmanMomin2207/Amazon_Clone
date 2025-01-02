a48570d7dbc5cf68a32f15513e4559bde917d4db
mominAman7860 <amanchandsomomin7860@gmail.com>
2025-01-02 23:15:31 +0530

    Covert loadProducts form XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,14 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {renderCheckoutHeader} from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 // import '../data/car.js';
 //import '../data/cart-class.js';
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value1');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
@@ -20,7 +16,7 @@ Promise.all([
     })
 
 ]).then((values) => {
-    console.log(values);
+    console.log(values); 
     renderCheckoutHeader();
     renderPaymentSummary();
     renderOrderSummary();
